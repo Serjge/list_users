c98efac3229d9964cb668595cf868e85dd1ca65d
Serjge <serjge@yandex.ru>
2022-04-23 11:30:08 +0300

    feat: add button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,8 +2,11 @@ import { FC } from 'react';
 
 import style from './app.module.scss';
 
+import { Button } from 'components';
+
 export const App: FC = () => (
   <div className={style.wrapper}>
-    <h1 className={style.title}>Hotel</h1>
+    <h1 className={style.title}>User list</h1>
+    <Button onClick={() => {}}>Редактировать</Button>
   </div>
 );
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,30 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps, FC } from 'react';
+
+import styles from './Button.module.scss';
+
+type ButtonPropsType = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
+  typeButton?: 'default' | 'edit';
+  isDisable?: boolean;
+};
+
+export const Button: FC<ButtonPropsType> = ({
+  typeButton,
+  isDisable,
+  onClick,
+  type,
+  children,
+}) => {
+  const style =
+    typeButton === 'edit' ? `${styles.button} ${styles.button__edit}` : styles.button;
+
+  return (
+    <div>
+      <button disabled={isDisable} onClick={onClick} type={type} className={style}>
+        {children}
+      </button>
+    </div>
+  );
+};
